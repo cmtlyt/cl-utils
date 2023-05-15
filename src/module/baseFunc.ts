2ed27f476a68bbda964a85075109898d91c66347
cmtlyt <cmtlyt@163.com>
2023-05-15 13:26:52 +0800

    20230515-新增获取对象选择器方法

--- src/module/baseFunc.ts
+++ src/module/baseFunc.ts
@@ -151,3 +151,54 @@ export function createUUID(): string {
   const mask = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
   return mask.replace(/x/g, () => ((Math.random() * 64) >> 1).toString(32))
 }
+
+export function getDOMSelector($dom: TSome<Node>): string {
+  function __getDomSelector($dom: Node): string {
+    const domTag: string = $dom.nodeName.toLowerCase()
+    if ($dom instanceof Element) {
+      if ($dom.id) {
+        return domTag + `#${$dom.id}`
+      } else if ($dom.classList.length) {
+        return domTag + `.${Array.from($dom.classList).join('.')}`
+      }
+      const idx: number = [...($dom.parentNode?.children || [])].findIndex(item => item === $dom)
+      if (~idx) {
+        return `${domTag}:nth-child(${idx + 1})`
+      }
+      console.warn('当前选择不准确')
+      return domTag
+    }
+    return ''
+  }
+  function __getSelectors($dom: TSome<Node>, suffix: string = ''): string {
+    if (!$dom || ~suffix.indexOf('#')) {
+      return suffix
+    }
+    return __getSelectors($dom.parentElement, `${__getDomSelector($dom)}>${suffix}`)
+  }
+  return __getSelectors($dom).slice(0, -1)
+}
+
+export function getDOMXPath($dom: TSome<Node>): string {
+  if ($dom instanceof Element) {
+    if ($dom.id !== '') {
+      return `//*[@id="${$dom.id}"]`
+    }
+    if ($dom === document.body) {
+      return $dom.nodeName
+    }
+    let ix = 0
+    let siblings = $dom.parentNode?.childNodes || []
+    for (let i = 0; i < siblings.length; i++) {
+      let sibling = siblings[i]
+      if (sibling === $dom) {
+        return `${getDOMXPath($dom.parentNode)}/${$dom.nodeName}[${ix + 1}]`
+      }
+      if (sibling.nodeType === 1 && sibling.nodeName === $dom.nodeName) {
+        ++ix
+      }
+    }
+    return ''
+  }
+  return ''
+}
